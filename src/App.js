c44d1509cfca0958bc402fb321f422c13bc3ea89
Juan Gomez <juancarlos.gm931@gmail.com>
2023-02-21 10:05:36 -0500

    Se da estilo al searchbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import Card from './components/Card.jsx';
 import Cards from './components/Cards.jsx';
+import SearchBar from './components/SearchBar.jsx';
 import characters, { Rick } from './data.js';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer';
 
+
 function App() {
 	return (
 		<div className='App'>
-			<Header
-				className='header-title'
-				title=''
-			/>
+			<Header className='header-title' title='' />
+			<SearchBar onSearch={(characterID) => window.alert(characterID)} />
 			<Card
 				name={Rick.name}
 				species={Rick.species}
