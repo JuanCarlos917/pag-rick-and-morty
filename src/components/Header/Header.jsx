b77659c27b169fd99713dede88f266a490d7de5d
Juan Gomez <juancarlos.gm931@gmail.com>
2023-03-02 08:44:48 -0500

    Se configura visualmente Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,27 +3,26 @@ import styles from './Header.module.css';
 import SearchBar from '../SearchBar/SearchBar';
 import { NavLink } from 'react-router-dom';
 
-
 export default function Header(props) {
 	return (
 		<header className={styles.header}>
-            <NavLink to='/home' >
-			    <img
-				    className={styles.logorickandmorty}
-				    src={logo_rick_and_morty}
-				    alt={props.name}
-			    />
-            </NavLink>
-            <NavLink to='/home'>
-			<p> home </p>
-            </NavLink>
-            <NavLink to='/about'>
-                <p>About</p>
-            </NavLink>
-            <NavLink to='/'>
-                <p>Logout</p>
-            </NavLink>
-			    <SearchBar onSearch={props.onSearch} />
+			<NavLink to='/home'>
+				<img
+					className={styles.logorickandmorty}
+					src={logo_rick_and_morty}
+					alt={props.name}
+				/>
+			</NavLink>
+			<NavLink to='/home'>
+				<p> home </p>
+			</NavLink>
+			<NavLink to='/about'>
+				<p>About</p>
+			</NavLink>
+			<NavLink to='/'>
+				<p>Logout</p>
+			</NavLink>
+			<SearchBar onSearch={props.onSearch} />
 		</header>
 	);
 }
